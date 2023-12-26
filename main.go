52eae7dd05fd4d256e982b1e6988dcd733b0f602
Simon Brady <simon@hikari.org.nz>
2023-12-26 13:40:20 +1300

    Update to go 1.20

--- main.go
+++ main.go
@@ -9,25 +9,24 @@ import (
 	"github.com/simonbrady/go-plugin-example/hello"
 )
 
-func check(err error) {
+// Generic wrapper for any function with error as its second return type,
+// so the caller doesn't have to worry about error handling.
+func must[T any](retval T, err error) T {
 	if err != nil {
 		panic(err)
 	}
+	return retval
 }
 
 func main() {
-	execPath, err := os.Executable()
-	check(err)
-	pluginPaths, err := filepath.Glob(filepath.Join(filepath.Dir(execPath), "plugins", "*.so"))
-	check(err)
+	execPath := must(os.Executable())
+	pluginPaths := must(filepath.Glob(filepath.Join(filepath.Dir(execPath), "plugins", "*.so")))
 	var plugins []hello.Hello
 	for _, pluginPath := range pluginPaths {
 		fmt.Printf("Loading %s\n", pluginPath)
-		p, err := plugin.Open(pluginPath)
-		check(err)
+		p := must(plugin.Open(pluginPath))
 		// Call factory method to get plugin's implementation of the hello.Hello interface
-		getHello, err := p.Lookup("GetHello")
-		check(err)
+		getHello := must(p.Lookup("GetHello"))
 		plugins = append(plugins, getHello.(func() hello.Hello)())
 	}
 	// Invoke plugins through the common interface
