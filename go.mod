6da77caa641bb2c298df87c68a5953776f087f62
Simon Brady <simon@hikari.org.nz>
2023-12-26 13:37:04 +1300

    Update to go 1.20

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/simonbrady/go-plugin-example
 
-go 1.16
+go 1.20
--- main.go
+++ main.go
@@ -7,23 +7,22 @@ import (
 	"plugin"
 )
 
-func check(err error) {
+// Generic wrapper for any function with error as its second return type,
+// so the caller doesn't have to worry about error handling.
+func must[T any](retval T, err error) T {
 	if err != nil {
 		panic(err)
 	}
+	return retval
 }
 
 func main() {
-	execPath, err := os.Executable()
-	check(err)
-	pluginPaths, err := filepath.Glob(filepath.Join(filepath.Dir(execPath), "plugins", "*.so"))
-	check(err)
+	execPath := must(os.Executable())
+	pluginPaths := must(filepath.Glob(filepath.Join(filepath.Dir(execPath), "plugins", "*.so")))
 	for _, pluginPath := range pluginPaths {
 		fmt.Printf("Loading %s\n", pluginPath)
-		p, err := plugin.Open(pluginPath)
-		check(err)
-		hello, err := p.Lookup("Hello")
-		check(err)
+		p := must(plugin.Open(pluginPath))
+		hello := must(p.Lookup("Hello"))
 		fmt.Println(hello.(func() string)())
 	}
 }
